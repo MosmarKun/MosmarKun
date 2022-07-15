17abf255eb88bae4780fe42c446eae76e5b2eac9
MadJade <109320458+MadJade@users.noreply.github.com>
2022-07-15 06:20:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,24 @@ from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 import sqlite3
 
+
 conn = sqlite3.connect('User.db',check_same_thread=False)
 c = conn.cursor()
 app = FastAPI()
 users = {}
+conn.commit()
+
 
 @app.get("/registerNewUser")
 def registerNewUser(name,age,gender,major):
     c.execute("insert into users values (?,?,?,?)",(name,age,gender,major))
-    print("values should be added successfully!")
+    conn.commit()
     return name + ' Has been created'
 
 @app.get("/selectUsers")
 def deleteUser():
     c.execute("select * from users")
     users = c.fetchall()
+    conn.commit()
     return users
+    
