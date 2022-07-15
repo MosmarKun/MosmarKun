7cc50f8f8ab1922d0041fe9e098184e5605eaf08
MadJade <109320458+MadJade@users.noreply.github.com>
2022-07-15 05:56:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,9 @@ def registerNewUser(name,age,gender,major):
     c.execute("insert into users values (?,?,?,?)",(name,age,gender,major))
     print("values should be added successfully!")
     return name + ' Has been created'
+
+@app.get("/selectUsers")
+def deleteUser():
+    c.execute("select * from users")
+    users = c.fetchall()
+    return users
